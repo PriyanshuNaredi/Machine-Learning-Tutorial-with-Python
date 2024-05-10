1393a4f31191e02a305b38c9a2a62ced5e90e474
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-05-10 12:24:22 +0530

    Completed  text_classification Neural Network

--- Machine Learning with Python/Neural Network/Tech with tim/text_classification.py
+++ Machine Learning with Python/Neural Network/Tech with tim/text_classification.py
@@ -1,3 +1,4 @@
+
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 #
@@ -33,14 +34,65 @@ train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_i
 test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding='post', maxlen= 250)
 
 # print(len(test_data[0]),len(test_data[1]))
-
+"""
 # Model
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000,16))
+model.add(keras.layers.Embedding(90000,16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16,activation = 'relu'))
 model.add(keras.layers.Dense(1,activation='sigmoid'))
 
 model.summary()  # prints a summary of the model
 
+model.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
+
+
+model.save("Machine Learning with Python/Neural Network/Tech with tim/model.h5")
+"""
+
+# Test
+# test_review = test_data[0]
+# predict = model.predict([test_data[0]])
+# print('Review: ')
+# print(decode_review(test_review))
+# print('Prediction:' +str(predict[0]))
+# print('Actual:' + str(test_labels[0]))
+# print(results)
+
+model = keras.models.load_model("Machine Learning with Python/Neural Network/Tech with tim/model.h5")
+
+# model = keras.models.load_model("my_model.keras")
+
+def review_encode(s):
+	encoded = [1]
+
+	for word in s:
+		if word.lower() in word_index:
+			encoded.append(word_index[word.lower()])
+		else:
+			encoded.append(2)
+
+	return encoded
+
+with open("Machine Learning with Python/Neural Network/Tech with tim/test_250_words.txt", encoding="utf-8") as f:
+	for line in f.readlines():
+		nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"","").strip().split(" ")
+		encode = review_encode(nline)
+		encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250) # make the data 250 words long
+		predict = model.predict(encode)
+		print("Original Text"+line)
+		print("Encoded text"+encode)
+		print("Prediction: "+predict[0])
 
