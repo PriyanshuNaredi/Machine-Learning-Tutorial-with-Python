1e1683d8f75bf7e7d4eaa4d9408b7f67a40659f0
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-06-13 21:48:58 +0530

    KNN ->breast_cancer.py

--- Model Building/KNN/breast_cancer.py
+++ Model Building/KNN/breast_cancer.py
@@ -18,50 +18,59 @@ def k_nearest_neighbor(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     # print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
     
-    return vote_result
+    # print(vote_result,confidence)
+    return vote_result, confidence
 
 
-df = pd.read_csv('Machine Learning with Python/KNN/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], axis=1, inplace=True)
+accuracies = []
+    
+for i in range(25):
+    df = pd.read_csv('Machine Learning with Python/KNN/breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], axis=1, inplace=True)
 
-full_data = df.astype(float).values.tolist()
+    full_data = df.astype(float).values.tolist()
 
-print(full_data[:5])
-print(20*'*')
-random.shuffle(full_data)
-print(full_data[:5])
+    print(full_data[:5])
+    print(20*'*')
+    random.shuffle(full_data)
+    print(full_data[:5])
 
-test_size = 0.2
-train_set = {2:[], 4:[]}
-test_set = {2:[],4:[]}
+    test_size = 0.4
+    train_set = {2:[], 4:[]}
+    test_set = {2:[],4:[]}
 
-train_data = full_data[:-int(test_size * len(full_data))]
-test_data = full_data[-int(test_size * len(full_data)) :] # last 20% of full data
+    train_data = full_data[:-int(test_size * len(full_data))]
+    test_data = full_data[-int(test_size * len(full_data)) :] # last 20% of full data
 
-for i in train_data:
-    """train_set[i[-1]] find last col in the data and fills train set in either 2 or 4 """
-    train_set[i[-1]].append(i[:-1])
+    for i in train_data:
+        """train_set[i[-1]] find last col in the data and fills train set in either 2 or 4 """
+        train_set[i[-1]].append(i[:-1])
 
-for i in test_data:
-    """train_set[i[-1]] find last col in the data and fills train set in either 2 or 4 """
-    test_set[i[-1]].append(i[:-1])
-
-
-correct = 0
-total = 0
-
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbor(data=train_set, predict=data, k=5)
-        if group == vote:
-            correct += 1
-        total += 1
+    for i in test_data:
+        """train_set[i[-1]] find last col in the data and fills train set in either 2 or 4 """
+        test_set[i[-1]].append(i[:-1])
 
 
-print("accuracy:",correct/total)
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote,confidence = k_nearest_neighbor(data=train_set, predict=data, k=5)
+            if group == vote:
+                correct += 1
+            else:
+                print(confidence)
+            total += 1
+            
+            
 
 
+    # print("accuracy:",correct/total)
+    accuracies.append(correct/total)
 
+print(sum(accuracies)/len(accuracies))
 
